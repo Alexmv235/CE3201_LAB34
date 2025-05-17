0bd925c82b35e48fe594af91e6af0909a6bb41a5
Alexander Montero V <alexmv223@pm.me>
2025-05-17 00:29:09 -0600

    [FIX] identacion

--- Ejercicios/Ejercicio_2.s
+++ Ejercicios/Ejercicio_2.s
@@ -14,16 +14,16 @@
 // _______________________________________________________
 _start:
 	MOV R2, #0x1000		// Dirección base del arreglo
-	
+
 	MOV R1, #3			// Valor#1 = 3
 	BL factorial_x
 	STR R0, [R2,#0]		// Salida#1 = 0x6
-	
+
 	MOV R1, #7			// Valor#2 = 7
 	BL factorial_x
 	STR R0, [R2,#4]		// Salida#2 = 0x13B0
-	
-	
+
+
 	MOV R1, #10			// Valor#3 = 10
 	BL factorial_x
 	STR R0, [R2,#8]		// Salida#3 = 0x375F00
@@ -45,7 +45,7 @@ _start:
 //      R0: registro - Resultado del factorial
 // _____________________________________________________
 factorial_x:
-    MOV R0, #1
+	MOV R0, #1
 	
 loop:
 	CMP R1,#1			// Compara si el valor es 1, ya que el factorial de 1 es 1 y ahí termina la recursión
